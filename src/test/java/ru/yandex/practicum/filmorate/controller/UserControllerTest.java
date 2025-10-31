7884e2b859769b206872de8db5f15458c55966f8
Fufurist <hopilki.popito@gmail.com>
2025-11-01 01:22:16 +0300

    Feature: UserControllerTest

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -0,0 +1,84 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exceptions.InappropriateInputException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.InvalidPropertiesFormatException;
+
+public class UserControllerTest {
+    private UserController controller;
+
+    @BeforeEach
+    public void setup() {
+        controller = new UserController();
+    }
+
+    @Test
+    public void voidLogin() {
+        User user = new User();
+        user.setEmail("aaa@");//"golder.ok@mail.com");
+        user.setName("");
+        user.setLogin(null);
+        user.setBirthday(LocalDate.of(2001, 5, 23));
+        try {
+            controller.create(user);
+        } catch (InappropriateInputException e) {
+            return;
+        } catch (Throwable e) {
+            Assertions.fail("Random exception ", e);
+        }
+        Assertions.fail("Login somehow slipped being empty");
+    }
+
+    @Test
+    public void emailValidation(){
+        User user = new User();
+        user.setEmail("aaa@");//"golder.ok@mail.com");
+        user.setName("");
+        user.setLogin("coller");
+        user.setBirthday(LocalDate.of(2001, 5, 23));
+        try {
+            controller.create(user);
+        } catch(Throwable e) {
+            Assertions.fail("Unexpected error", e);
+        }
+        user.setEmail(null);
+        try {
+            controller.create(user);
+        } catch(InappropriateInputException e){
+            try {
+                user.setEmail("noDoggo");
+                controller.create(user);
+            } catch (InappropriateInputException e2) {
+                return;
+            } catch (Throwable e2) {
+                Assertions.fail("Unexpected error", e);
+            }
+            Assertions.fail("Email without @ slipped");
+        } catch(Throwable e) {
+            Assertions.fail("Unexpected error", e);
+        }
+        Assertions.fail("Blank email slipped");
+    }
+
+    @Test
+    public void birthDayTooLate(){
+        User user = new User();
+        user.setEmail("aaa@");//"golder.ok@mail.com");
+        user.setName("");
+        user.setLogin("coller");
+        user.setBirthday(LocalDate.of(2030, 5, 23));
+        try {
+            controller.create(user);
+        } catch (InappropriateInputException e) {
+            return;
+        } catch (Throwable e) {
+            Assertions.fail("Random exception ", e);
+        }
+        Assertions.fail("Date slipped to be in 2030");
+    }
+}
